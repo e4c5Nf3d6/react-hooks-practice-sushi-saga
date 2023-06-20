bc46fe9a16b6ee4de81ed71a364a3fdec60be2de
Maria Jenkins <macjenkin@gmail.com>
2023-06-19 19:23:09 -0500

    complete challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [moneyLeft, setMoneyLeft] = useState(100)
+  const [plates, setPlates] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(data => setSushis(data))
+  }, [])
+
+  function eatSushi(sushi) {
+    setPlates([...plates, sushi])
+    setMoneyLeft(moneyLeft => moneyLeft - sushi.price)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} onEatSushi={eatSushi} moneyLeft={moneyLeft} />
+      <Table plates={plates} moneyLeft={moneyLeft} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onEatSushi, moneyLeft }) {
+  const [startId, setStartId] = useState(1)
+
+  function loadMore() {
+    setStartId(startId => startId + 4)
+  }
+
+  const sushisToDisplay = sushis.filter(sushi => {
+    return sushi.id >= startId && sushi.id < startId + 4
+  })
+
+  const sushiComponents = sushisToDisplay.map(sushi => {
+    return <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} moneyLeft={moneyLeft} />
+  })
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onLoadMore={loadMore} />
     </div>
   );
 }
